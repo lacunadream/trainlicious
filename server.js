a599603f1f9239513fd7c2dafab48aa66fd64b18
clouddream <d.s.yong@lse.ac.uk>
2015-11-21 23:10:34 +0000

    added postgre

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var fs = require('fs');
+var pg = require('pg');
  
  var app = express()
 // App shit
@@ -11,6 +11,34 @@ app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
+// Postgre
+
+var conString = "postgres://oxbqavxj:dx6CgdjYT_IVp2YvxXzLJM_uX7yMOY9x@horton.elephantsql.com:5432/oxbqavxj";
+
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 20 (also configurable)
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM weather', function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+});
+
+
+
+
+
+// Routes
 app.get('/', function(req, res) {
     res.json({ message: 'eff off' });   
 });
