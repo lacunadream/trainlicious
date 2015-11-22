77829790d1e64729ff37c53b59d96f997a26df5a
clouddream <d.s.yong@lse.ac.uk>
2015-11-22 01:42:12 +0000

    proper string handling - heh

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var fs = require('fs')
  
  var app = express()
 // App shit
@@ -12,8 +13,9 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
 // Postgre
-
-var conString = "postgres://postgres:qwertyui@localhost:5432/postgres";
+var secrets = fs.readFileSync('details.txt', 'utf-8').toString()
+console.log(secrets)
+var conString = secrets;
 
 //this initializes a connection pool
 //it will keep idle connections open for a (configurable) 30 seconds
@@ -35,11 +37,26 @@ var conString = "postgres://postgres:qwertyui@localhost:5432/postgres";
 // });
 
 
-
 // Routes
 app.get('/', function(req, res) {
     res.json({ message: 'eff off' });   
 });
+
+// pg.connect(conString, function(err, client, done) {
+//   if(err) {
+//     return console.error('error fetching client from pool', err);
+//   }
+//   client.query('SELECT * FROM weather', function(err, result) {
+//     //call `done()` to release the client back to the pool
+//     done();
+
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result);
+//     //output: 1
+//   });
+// });
 
 app.get('/api/query', function (req, res) {
     var dest = req.query.dest;
@@ -52,20 +69,21 @@ app.get('/api/query', function (req, res) {
 		if(err) {
 	    	return console.error('error fetching client from pool', err);
 	 	}
-		client.query('SELECT COUNT(*) FROM weather', function(err, result){
+		client.query('SELECT COUNT(*) FROM webcam', function(err, result){
 		  	if (err) {
 		  		return console.log('count1 ' + err);
 		  	}
 		  	console.log(result.rows[0].count)
 		  	var tableRow = result.rows[0].count - 1
+		  	// var tableRow = 1
 		  	console.log('asd ' + tableRow)
-		  	client.query('SELECT * FROM weather', function(err, result2) {
+		  	client.query('SELECT * FROM webcam', function(err, result2) {
 		    done();
 
 		    if(err) {
 		      return console.error('error running query', err);
 		    }
-		    var lol = result2.rows[tableRow].temp_lo
+		    var lol = result2.rows[tableRow].value
 		    console.log('RAWR ' + lol)
 		        var x = {"CrowdScore":lol, 
 		    	"Departure":dept, 
@@ -78,9 +96,6 @@ app.get('/api/query', function (req, res) {
 		  })
 	});
 })
-
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
